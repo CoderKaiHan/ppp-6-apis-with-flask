c1b6c680d723cecea3809ba7bea8e25d443f58f0
CoderKaiHan <hankaicoder@gmail.com>
2024-05-18 12:50:36 -0400

    Define to_json function in models && test api routes

--- reptiles/blueprints/reptile.py
+++ reptiles/blueprints/reptile.py
@@ -1,4 +1,4 @@
-from flask import ( Blueprint, render_template, request, jsonify)
+from flask import ( Blueprint, request, jsonify)
 
 from ..models import models
 
--- reptiles/models/models.py
+++ reptiles/models/models.py
@@ -12,3 +12,13 @@ class Reptile(db.Model):
     conservation_status = db.Column(db.String(250), nullable=False)
     native_habitat = db.Column(db.String(250), nullable=False)
     fun_facts = db.Column(db.Text, nullable=True)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'common_name': self.common_name,
+            'scientificf_name': self.scientificf_name,
+            'conservation_status': self.conservation_status,
+            'native_habitat': self.native_habitat,
+            'fun_facts': self.fun_facts
+        }
